1d9f0ce26f54472495ce252bac7209550955f2b3
Fck-r-sns <fck.r.sns@gmail.com>
2016-03-20 00:29:19 +0300

    UpdaterService added

--- app/src/main/java/com/binary_machinery/avalonschedule/SettingsActivity.java
+++ app/src/main/java/com/binary_machinery/avalonschedule/SettingsActivity.java
@@ -1,8 +1,14 @@
 package com.binary_machinery.avalonschedule;
 
 import android.app.ActionBar;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -14,5 +20,32 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        final Intent serviceLaunchIntent = new Intent(SettingsActivity.this, UpdaterService.class);
+        final PendingIntent pendingIntent = PendingIntent.getService(SettingsActivity.this, 0, serviceLaunchIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        final AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Button startServiceButton = (Button) findViewById(R.id.start_service_button);
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                startService(new Intent(SettingsActivity.this, UpdaterService.class));
+                am.setInexactRepeating(
+                        AlarmManager.ELAPSED_REALTIME,
+                        0,
+                        10000,
+                        pendingIntent
+                );
+            }
+        });
+
+        Button stopServiceButton = (Button) findViewById(R.id.stop_service_button);
+        stopServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                am.cancel(pendingIntent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/binary_machinery/avalonschedule/UpdaterService.java
@@ -0,0 +1,59 @@
+package com.binary_machinery.avalonschedule;
+
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v7.app.NotificationCompat;
+import android.widget.Toast;
+
+import com.binary_machinery.avalonschedule.data.GlobalEnvironment;
+import com.binary_machinery.avalonschedule.tools.DbProvider;
+import com.binary_machinery.avalonschedule.tools.ScheduleUpdater;
+
+import java.util.Calendar;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+
+public class UpdaterService extends Service {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        createNotification();
+        GlobalEnvironment env = GlobalEnvironment.getInstance();
+        if (env.dbProvider == null) {
+            env.dbProvider = new DbProvider(this);
+        }
+//        String sourceUrl = "http://www.avalon.ru/HigherEducation/MasterProgrammingIS/Schedule/Semester3/Groups/?GroupID=12285";
+        String sourceUrl = "http://www.avalon.ru/HigherEducation/MasterProgrammingIS/Schedule/Semester1/Groups/?GroupID=12085";
+
+        Observable.just(sourceUrl)
+                .concatMap(ScheduleUpdater::get)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        schedule -> {
+                        },
+                        throwable -> Toast.makeText(this, R.string.task_loading_failed, Toast.LENGTH_SHORT).show(),
+                        () -> Toast.makeText(this, R.string.task_loading_finished, Toast.LENGTH_SHORT).show()
+                );
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw null;
+    }
+
+    private void createNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setAutoCancel(true);
+        builder.setContentTitle("Notification");
+        builder.setContentText(Calendar.getInstance().getTime().toString());
+
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.notify((int) Calendar.getInstance().getTimeInMillis(), builder.build());
+    }
+}
